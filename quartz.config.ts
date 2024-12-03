cf3b6a7d00d0a0f65839ca6fcf88707ef8f658f6
Joan GE <jongares@hotmail.com>
2024-12-03 01:30:13 +0100

    Quartz sync: Dec 3, 2024, 1:30 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "es-ES",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://j6e.github.io/La-Isla-de-los-Grifos/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
